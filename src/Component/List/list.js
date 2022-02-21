fe59a8314628963f49e911ab66d02917febfb044
AmanpreetSingh98 <amanpreetsinghc14@gmail.com>
2022-02-21 18:12:59 +0530

    hm

--- src/Component/List/list.js
+++ src/Component/List/list.js
@@ -10,7 +10,7 @@ const List = () => {
     }
 
     const createField = (event) => {
-        
+        document.getElementById('text').innerText=event.keyCode;
         if(event.code == 'Enter' || event.code == 'NumpadEnter' || event.keyCode == 13){
             
             var box = document.createElement('div');
@@ -23,34 +23,54 @@ const List = () => {
             var field = document.createElement('input');
             field.id=n;
             field.className='ibox';
+            var btn = document.createElement('button');
+            btn.className='cross';
+            btn.appendChild(document.createTextNode("✕"));
             box.appendChild(check);
             box.appendChild(field);
+            box.appendChild(btn);
             document.getElementById('list-box').appendChild(box);
             document.getElementById(Number(event.target.id)+1).children[1].focus();
             document.getElementById(n).children[0].addEventListener('change',strike);
             document.getElementById(n).addEventListener('keydown',createField);
+            document.getElementById(n).children[2].addEventListener('click',removeNote);
             n++;
         }
     }
     const strike = (event) => {
         var check = document.getElementById(event.target.id).children[1].style.textDecoration;
         if(check == '' || check == 'none'){
+            document.getElementById('checked').append(document.getElementById(event.target.id))
             document.getElementById(event.target.id).children[1].style.textDecoration='line-through'
         }
         else{
+            document.getElementById('list-box').append(document.getElementById(event.target.id))
             document.getElementById(event.target.id).children[1].style.textDecoration='none'
         }
+    }
+    const removeNote = (event) => {
+        console.log(event)
+        document.getElementById(event.nativeEvent.path[1].id).remove()
+    }
+    const drag = (event) => {
+
     }
     return(
         <div className='container'>
             <div className='row mt-3'>
                 <input placeholder='Title..&#9999;' onChange={saveTitle} className='w-75 m-auto'></input>
+                <p id="text"></p>
                 <div id="list-box">
-                    <div id="1" className='mb-2'>
+                    <div id="1" className='mb-2' draggable="true" onDragStart={drag}>
                         <input type="checkbox" onChange={strike} id="1" className='form-check-input cbox'></input>
                         <input onKeyDown={createField} id='1' className='ibox'></input>
+                        <button className='cross' onClick={removeNote}>	&#10005;</button>
                     </div>
-                    
+                </div>
+                <div id="checked">
+                    <hr/>
+                    <p>checked</p>
+
                 </div>
             </div>
         </div>
