d7b870fc5e915c92725a585d2225e5ccf55de489
AmanpreetSingh98 <amanpreetsinghc14@gmail.com>
2022-04-06 16:47:41 +0530

    check

--- src/Component/Practice/practice.js
+++ src/Component/Practice/practice.js
@@ -4,7 +4,14 @@ import './practice.css'
 const Ptc = () => {
     const device = () => {
         let details= navigator.userAgent;
-        return details.split('(')[1].split(';')[0]
+        // return details.split('(')[1].split(';')[0]
+        let regexp = /Android/i;
+        if(regexp.test(details)){
+            return('This is a Mobile Device')
+        }
+        else{
+            return('This is a Desktop')
+        } 
     }
     return(
         <div className='container mt-3'>
@@ -15,6 +22,8 @@ const Ptc = () => {
                 
             </div>
             {device()}
+            <hr/>
+            {navigator.userAgent.split('(')[1].split(';')[0]}
             <hr/>
             {navigator.userAgent}
         </div>
